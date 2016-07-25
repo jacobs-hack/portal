0109df5ecd3ee2eac492f15f55cdf7c6ba5fe9c6
Natnael Getahun <natnael.wubishet.21@gmail.com>
2016-07-25 17:10:07 +0300

    Added facebook auth routes

--- server/auth/index.js
+++ server/auth/index.js
@@ -12,4 +12,15 @@ var router = express.Router();
 
 router.use('/local', require('./local'));
 
+/* facebook auth */
+router.use('/facebook', passport.authenticate('facebook'));
+
+/* callback function*/
+router.use('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+    console.log("Successfully logged in.");
+});
+
 module.exports = router;
